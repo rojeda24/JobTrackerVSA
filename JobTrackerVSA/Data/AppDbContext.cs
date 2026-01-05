a95ac14a62c6f83e275eb9a2c6ea4a2931c1af73
Raul Ojeda <raul.ojeda.luna@gmail.com>
2026-01-05 13:41:55 -0700

    Initial configuration

---
+++ JobTrackerVSA/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using JobTrackerVSA.Web.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobTrackerVSA.Web.Data
+{
+    public class AppDbContext (DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<JobApplication> JobApplications => Set<JobApplication>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<JobApplication>()
+                .Property(j => j.CompanyName).HasMaxLength(200);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ JobTrackerVSA/Domain/JobApplication.cs
@@ -0,0 +1,25 @@
+﻿namespace JobTrackerVSA.Web.Domain
+{
+    public class JobApplication
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public required string CompanyName { get; set; }
+        public required string Position { get; set; }
+        public string? JobDescriptionUrl { get; set; }
+        public DateTime AppliedAt { get; set; } = DateTime.UtcNow;
+        public ApplicationStatus Status { get; set; }
+        public string? Notes { get; set; }
+
+
+        public enum ApplicationStatus
+        {
+            Applied,
+            Interviewing,
+            TechnicalTest,
+            Offered,
+            Rejected,
+            Accepted
+        }
+    }
+}
