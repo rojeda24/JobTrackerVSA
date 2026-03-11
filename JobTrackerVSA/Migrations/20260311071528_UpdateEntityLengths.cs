9ff411b0f31d7822e0569251a23571b8b764d0dd
Raul Ojeda <raul.ojeda.luna@gmail.com>
2026-03-11 01:17:13 -0600

    fix(db): add missing migration for updated entity lengths

---
+++ JobTrackerVSA/Migrations/20260311071528_UpdateEntityLengths.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTrackerVSA.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEntityLengths : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "JobApplications",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldMaxLength: 450);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobDescriptionUrl",
+                table: "JobApplications",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "JobApplications",
+                type: "nvarchar(450)",
+                maxLength: 450,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "JobDescriptionUrl",
+                table: "JobApplications",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(2048)",
+                oldMaxLength: 2048,
+                oldNullable: true);
+        }
+    }
+}
